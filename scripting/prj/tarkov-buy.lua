348ffa6714386ac9f1f4de0882fded82f3763a41
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2026-01-08 02:15:51 +0700

    looks good

--- scripting/prj/tarkov-buy.lua
+++ scripting/prj/tarkov-buy.lua
@@ -1,7 +1,21 @@
--- data = dofile[[F:\home\koshi8bit\soft\windows\01-main\logitech\scripting\prj\tarkov-buy.lua]]
 function OnEvent(event, arg)
 	OutputLogMessage("event = %s, arg = %s\n", event, arg);
 	if (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
-		MoveMouseTo(1000, 1000)
+		MoveMouseTo(60951, 8198)
+		Sleep(10)
+		PressAndReleaseMouseButton(1)
+		Sleep(20)
+		MoveMouseTo(58748, 11750)
+	end
+
+	if (event == "MOUSE_BUTTON_PRESSED" and arg == 8) then
+		PressAndReleaseMouseButton(1)
+		Sleep(50)
+		PressAndReleaseKey("y")
+	end
+
+	if (event == "MOUSE_BUTTON_PRESSED" and arg == 6) then
+		x, y = GetMousePosition();
+		OutputLogMessage("Mouse is at %d, %d\n", x, y);
 	end
 end
