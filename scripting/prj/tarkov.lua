3470e61558da73d38f8538f9824ee2397ed61be4
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2026-01-09 16:06:54 +0700

    right+ctrl search

--- scripting/prj/tarkov.lua
+++ scripting/prj/tarkov.lua
@@ -3,7 +3,7 @@
 function OnEvent(event, arg)
 	OutputLogMessage("event = %s, arg = %s\n", event, arg);
 
-    if event == "MOUSE_BUTTON_PRESSED" and arg == 6 then
+    if event == "MOUSE_BUTTON_PRESSED" and arg == 4 then
 		MoveMouseTo(60951, 8198)
 		Sleep(10)
 		PressAndReleaseMouseButton(1)
@@ -11,8 +11,16 @@ function OnEvent(event, arg)
 		MoveMouseTo(58748, 11750)
     end
 
+	if event == "MOUSE_BUTTON_PRESSED" and arg == 2 and IsModifierPressed("ctrl") then
+		PressAndReleaseMouseButton(3)
+		Sleep(20)
+		MoveMouseRelative(20, 125)
+		Sleep(10)
+		PressAndReleaseMouseButton(1)
+    end
+
 	if (event == "MOUSE_BUTTON_PRESSED" and arg == 8) then
-		SetBacklightColor(255, 0, 0);
+		SetBacklightColor(255, 0, 0)
 		PressAndReleaseMouseButton(1)
 		Sleep(50)
 		PressAndReleaseKey("y")
